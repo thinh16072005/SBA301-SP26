378f59c1e3a00c1c5bb7ef0ab4b790e117b80159
Pham Hung Thinh <hungthinh16072005@gmail.com>
2026-01-06 10:42:41 +0700

    Push Ex1 - Slot 1

---
+++ Slot1/ex1/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+      <>
+          <Header/>
+          <Footer/>
+      </>
+  )
+}
+
+export default App
---
+++ Slot1/ex1/src/components/Header.jsx
@@ -0,0 +1,24 @@
+﻿import {Button, Container, Nav, Navbar} from "react-bootstrap";
+function Header() {
+    return (
+        <>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <br/>
+            <header>
+                <h1>Welcome to my website</h1>
+                <Button variant="primary">Click me</Button>
+            </header>
+        </>
+    )
+}
+
+export default Header
